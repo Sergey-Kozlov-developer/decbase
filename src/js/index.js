d33355bc198cdb3352de3e48610b30df47f86b77
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2023-11-22 17:17:07 +0300

    add home and swiper section

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+	slidesPerView: 3,
+	spaceBetween: 37,
+	navigation: {
+		prevEl: "#sliderPrev",
+		nextEl: "#sliderNext",
+	},
+});
