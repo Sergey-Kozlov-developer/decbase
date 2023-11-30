9e6a03bb0a07d6d7282274409f0accfae6186e49
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2023-11-30 16:22:47 +0300

    finish animations sections

--- src/js/modules/scrollReveal.js
+++ src/js/modules/scrollReveal.js
@@ -1,20 +1,28 @@
 import ScrollReveal from "scrollreveal";
 
 function scrollReveal() {
-	// базовые настройки
-	ScrollReveal({
-		distance: "60px",
-		duration: 2800,
-	});
-	ScrollReveal().reveal(`.home__block, .header`, {
-		origin: "top",
-	});
-	ScrollReveal().reveal(`.home__block`, {
-		origin: "bottom",
-	});
+    // базовые настройки
+    ScrollReveal({
+        distance: "60px",
+        duration: 2800,
+    });
+    ScrollReveal().reveal(`.home__block, .header, .service__top, .work__project, .wrap, .testimonials__title`, {
+        origin: "top",
+    });
+    ScrollReveal().reveal(`.home__block, .down, .subscribe__input`, {
+        origin: "bottom",
+    });
+    ScrollReveal().reveal(`.about__img, #sliderPrev`, {
+        origin: 'left',
+    });
+    ScrollReveal().reveal(`.about__desc, #sliderNext`, {
+        origin: 'right',
+    });
 
-	ScrollReveal().reveal(`.heftiba`, {
-		scale: 1.1,
-	});
+
+    ScrollReveal().reveal(`.heftiba`, {
+        scale: 1.1,
+    });
 }
+
 export default scrollReveal;
